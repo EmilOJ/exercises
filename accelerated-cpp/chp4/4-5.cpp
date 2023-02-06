0c4e9a1798925014e370a1d057a2a5525411b127
Emil Johansen <emil.johansen@gmail.com>
2023-02-06 16:10:33 +0900

    4-5

--- accelerated-cpp/chp4/4-5.cpp
+++ accelerated-cpp/chp4/4-5.cpp
@@ -1,3 +1,46 @@
 //
 // Created by emil on 2/6/23.
 //
+
+#include <istream>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+
+std::istream& read_words(std::istream& is, std::vector<std::string>& vec) {
+  std::string word;
+  if (is) {
+    while (is >> word) {
+      if (word == "end") {
+        break;
+      }
+      vec.push_back(word);
+    }
+  }
+
+  return is;
+}
+
+int main() {
+  std::vector<std::string> vec;
+  read_words(std::cin, vec);
+
+  // count number of words in input
+  std::cout << "Number of words: " << vec.size() << std::endl;
+
+  // count how many times each word occur
+  std::sort(vec.begin(), vec.end());
+  int cnt = 1;
+  for (int i = 0; i < vec.size(); ++i) {
+    if (vec[i] == vec[i+1]) {
+      cnt++;
+    } else {
+      std::cout << "\"" << vec[i] << "\": " << cnt << std::endl;
+      cnt = 1;
+    }
+  }
+
+  return 0;
+}
+
