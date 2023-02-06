ff84f8b93d00a1ecff955fcbf22494bf065f3e1f
Emil Johansen <emil.johansen@gmail.com>
2023-02-06 15:52:53 +0900

    refactor slightly

--- accelerated-cpp/chp4/4-2.cpp
+++ accelerated-cpp/chp4/4-2.cpp
@@ -7,11 +7,12 @@
 
 
 int main() {
-  std::string::size_type max_len = std::to_string(100*100).size();
+  int max_i = 1000;
+  std::string::size_type max_len = std::to_string(max_i * max_i).size();
   auto c_size = std::setw(max_len);
 
   std::cout << c_size << "i" << "\t" << c_size << "i*i" << std::endl;
-  for (int i = 1; i <= 100; ++i) {
+  for (int i = 1; i < max_i; ++i) {
     std::cout << c_size << i << "\t" << c_size << i*i << std::endl;
   }
 
